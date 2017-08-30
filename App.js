81c3c130cd89508b60b88538cf49c65ed46f5086
Jani Eväkallio <jani.evakallio@gmail.com>
2017-08-30 16:38:33 +0100

    Move card off the screen when nope/yep buttons are pressed

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Dimensions, StyleSheet, Text, View } from 'react-native';
 import data from './src/data';
 import Card from './src/components/Card';
 import Button from './src/components/Button';
+
+const SWIPE_DISTANCE = Dimensions.get('window').width;
 
 export default class App extends React.Component {
   state = {
     items: data
   };
+
+  position = new Animated.Value(0);
+
+  nopePressed = () => {
+    Animated.timing(this.position, {
+      toValue: -1
+    }).start();
+  };
+
+  yepPressed = () => {
+    Animated.timing(this.position, {
+      toValue: 1
+    }).start();
+  };
+
   render() {
     const item = this.state.items[0];
+
+    const translateX = this.position.interpolate({
+      inputRange: [-1, +1],
+      outputRange: [-SWIPE_DISTANCE, SWIPE_DISTANCE]
+    });
+
+    const rotate = this.position.interpolate({
+      inputRange: [-1, +1],
+      outputRange: ['-30deg', '30deg']
+    });
+
+    const animatedStyle = {
+      transform: [{ translateX }, { rotate }]
+    };
+
     return (
       <View style={styles.container}>
-        <Card image={item.image} text={item.text} />
+        <Animated.View style={animatedStyle}>
+          <Card image={item.image} text={item.text} />
+        </Animated.View>
         <View style={styles.buttonBar}>
-          <Button text="Nope" backgroundColor="#ff3b30" />
-          <Button text="Yep" backgroundColor="#4cd964" />
+          <Button onPress={this.nopePressed} text="Nope" backgroundColor="#ff3b30" />
+          <Button onPress={this.yepPressed} text="Yep" backgroundColor="#4cd964" />
         </View>
       </View>
     );
